f56be43b8949063005bbd22c57257e82c5c574ff
Kyrylo Pylinskyi <pylinskyi.k@e80group.com>
2023-07-20 18:44:08 +0200

    Google auth works

--- Api/Controllers/AuthControllers/GoogleController.cs
+++ Api/Controllers/AuthControllers/GoogleController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Identity;
 using Api.Models.Entities.Identity;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.Extensions.Options;
+using System.Security.Claims;
 
 namespace Api.Controllers.AuthControllers
 {
@@ -17,10 +18,12 @@ namespace Api.Controllers.AuthControllers
     [Route("api/[controller]")]
     public class GoogleController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public GoogleController(SignInManager<ApplicationUser> signInManager)
+        public GoogleController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
+            _userManager = userManager;
             _signInManager = signInManager;
         }
 
@@ -40,16 +43,51 @@ namespace Api.Controllers.AuthControllers
         [Route("GoogleResponse")]
         public async Task<IActionResult> GoogleResponse()
         {
-            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            var claims = result.Principal.Identities
-                .FirstOrDefault().Claims.Select(claim => new
+            var result = await HttpContext.AuthenticateAsync(GoogleDefaults.AuthenticationScheme);
+            if (result.Succeeded)
+            {
+                // Authentication successful, get the user's email from the claims
+                var userEmail = result.Principal.FindFirstValue(ClaimTypes.Email);
+                // Here, you can check if the user with this email already exists in your database.
+                // If the user exists, you can sign them in.
+                // If the user doesn't exist, you might consider creating a new user account.
+
+                // Assuming you have a method in your UserManager to find or create the user based on the email.
+                var user = await _userManager.FindByEmailAsync(userEmail);
+                if (user == null)
                 {
-                    claim.Issuer,
-                    claim.OriginalIssuer,
-                    claim.Type,
-                    claim.Value
-                });
-            return Ok(claims);
+                    // User does not exist in the database. You might create a new user account here.
+                    // Example:
+                    user = new ApplicationUser
+                    {
+                        UserName = userEmail.Substring(0, userEmail.IndexOf('@')),
+                        Email = userEmail,
+                        EmailConfirmed = true,
+                        // Set other user properties as needed.
+                    };
+
+                    // Create the new user in the database.
+                    var createResult = await _userManager.CreateAsync(user);
+                    if (!createResult.Succeeded)
+                    {
+                        // Handle user creation failure if needed.
+                        return BadRequest(new { msg = "User creation failed.", createResult });
+                    }
+                }
+                //else
+                //{
+                //    // If the user already exists, update the EmailConfirmed property to true.
+                //    user.EmailConfirmed = true;
+                //    await _userManager.UpdateAsync(user);
+                //}
+
+                // Sign in the user using the SignInManager.
+                await _signInManager.SignInAsync(user, isPersistent: false); // Change isPersistent as needed.
+
+                return Ok("User logged in successfully.");
+            }
+
+            return Unauthorized("Google authentication failed.");
         }
     }
 
---
+++ Api/Services/Security/JwtService.cs
@@ -0,0 +1,31 @@
+﻿//using Microsoft.AspNetCore.Authentication.OAuth;
+//using Microsoft.IdentityModel.Tokens;
+//using System.IdentityModel.Tokens.Jwt;
+//using System.Security.Claims;
+//using System.Text;
+
+//namespace Api.Services.Security
+//{
+//    public class JwtService
+//    {
+//        private string GetJwt(ClaimsIdentity claimsIdentity, TimeSpan expirence)
+//        {
+//            var tokenHandler = new JwtSecurityTokenHandler();
+//            var key = Encoding.UTF8.GetBytes(_authOptions.Key);
+
+//            var tokenDescriptor = new SecurityTokenDescriptor
+//            {
+//                Audience = _authOptions.Audience,
+//                Issuer = _authOptions.Issuer,
+//                Subject = claimsIdentity,
+//                Expires = DateTime.UtcNow.Add(expirence),
+//                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+//            };
+
+//            var token = tokenHandler.CreateToken(tokenDescriptor);
+//            var tokenString = tokenHandler.WriteToken(token);
+
+//            return tokenString;
+//        }
+//    }
+//}
